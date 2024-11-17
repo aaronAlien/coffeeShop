dc3c1458f11ab22c032393b289a03613e0d6733f
Aaron <thatshakespeare2@gmail.com>
2024-11-17 15:13:45 +0000

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("hamburger-btn").addEventListener("click", function () {
+  const navMenu = document.querySelector(".nav-left ul");
+  navMenu.classList.toggle("active");
+});
+
+// https://placehold.co/600x400
