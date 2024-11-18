8887cef3f2bbc943cb79ff317066b01920b9e9e8
Aaron <thatshakespeare2@gmail.com>
2024-11-18 11:11:50 +0000

    before navbar fix

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+
+    // HAMBURGER MENU
+
 document.getElementById("hamburger-btn").addEventListener("click", function () {
   const navMenu = document.querySelector(".nav-left ul");
   navMenu.classList.toggle("active");
 });
+
+    // HERO - CAROUSEL
+
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide-image');
+
+    let currentIndex = 0;
+
+    function nextSlide() {
+        //remove
+        slides[currentIndex].classList.remove('active');
+
+        //update
+        currentIndex = (currentIndex + 1) % slides.length;
+
+        //add
+        slides[currentIndex].classList.add('active');
+    }
+
+    setInterval(nextSlide, 3000);
+})
+
+
+    // MODAL
+
+
+
+
+
 
 // https://placehold.co/600x400
